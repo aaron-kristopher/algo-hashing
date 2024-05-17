9d61d601e68c6c91192f3f6dce525e541d736208
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-17 22:54:35 +0800

    feat: Implement resolveCollision method in CollisionResolution

--- Hashing/src/collision/CollisionResolution.java
+++ Hashing/src/collision/CollisionResolution.java
@@ -1,5 +1,9 @@
 package collision;
 
+import datastructures.SymbolTable;
+
 public abstract class CollisionResolution {
+
+    public abstract int resolveCollision(int index, String hashedValue, SymbolTable symbolTable);
 
 }
