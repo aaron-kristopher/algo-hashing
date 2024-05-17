133657df18c179bf2a4102c4249ebad18679ca54
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-17 22:53:09 +0800

    refactor: Implement hash method in AddAndFold and DigitSelection subclasses

--- Hashing/src/hash/AddAndFold.java
+++ Hashing/src/hash/AddAndFold.java
@@ -2,4 +2,15 @@ package hash;
 
 public class AddAndFold extends HashFunction {
 
+    @Override
+    public int hash(String key) {
+        int sum = 0;
+        String ASCIIValue = toASCIIValue(key);
+
+        for (int i = 0; i < ASCIIValue.length(); i++)
+            sum += Integer.parseInt(String.valueOf(ASCIIValue.charAt(i)));
+
+        return sum;
+    }
+
 }
--- Hashing/src/hash/DigitSelection.java
+++ Hashing/src/hash/DigitSelection.java
@@ -2,4 +2,9 @@ package hash;
 
 public class DigitSelection extends HashFunction {
 
+    @Override
+    public int hash(String key) {
+        return toASCIIValue(key).charAt(0);
+    }
+
 }
