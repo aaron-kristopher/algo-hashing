0efc8b35ce8d6148016a247f96e4f3461db352db
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-17 15:26:04 +0800

    Create a hash method to convert key to ASCII representation

--- Hashing/src/datastructures/SymbolTable.java
+++ Hashing/src/datastructures/SymbolTable.java
@@ -1,5 +1,23 @@
 package datastructures;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class SymbolTable {
 
+    private String[] table = new String[50];
+
+    public static void main(String[] args) {
+
+    }
+
+    public String hash(String key) {
+
+        String hash = key.chars()
+                .map(c -> (int) c) // Convert chars to int (ASCII)
+                .mapToObj(i -> String.valueOf(i)) // Convert int to String
+                .collect(Collectors.joining()); // Combine Strings
+
+        return hash;
+    }
 }
