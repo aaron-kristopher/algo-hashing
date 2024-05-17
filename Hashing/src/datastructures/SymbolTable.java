e4938ebfd71ae86aa1f860e07e08f4c4d9e147cb
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-17 22:54:01 +0800

    refactor: Implement put method with collision resolution in SymbolTable

--- Hashing/src/datastructures/SymbolTable.java
+++ Hashing/src/datastructures/SymbolTable.java
@@ -1,14 +1,32 @@
 package datastructures;
 
+import collision.CollisionResolution;
+import hash.HashFunction;
+
 public class SymbolTable {
 
     private String[] table = new String[50];
 
-    public void put(String key, int index) {
+    public void put(String key, int index, HashFunction hashFunction, CollisionResolution collisionResolution) {
+        String hashedValue = HashFunction.toASCIIValue(key);
+
+        while (!isCellEmpty(index))
+            index = collisionResolution.resolveCollision(index, hashedValue, this);
+
         table[index] = key;
     }
 
     public String get(int index) {
         return table[index];
+    }
+
+    private boolean isCellEmpty(int index) {
+        return table[index] == null;
+    }
+
+    public void printTable() {
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(i + ": " + get(i));
+        }
     }
 }
