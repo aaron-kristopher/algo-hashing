3bf5fde22a904a51f5e05eb3712599a6deca21e5
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-17 23:27:53 +0800

    refactor: Update hash method in Midsquare and ModuloArithmetic subclasses

--- Hashing/src/hash/Midsquare.java
+++ Hashing/src/hash/Midsquare.java
@@ -2,4 +2,9 @@ package hash;
 
 public class Midsquare extends HashFunction {
 
+    @Override
+    public int hash(String key) {
+        String ASCIIValue = toASCIIValue(key);
+        return Character.getNumericValue(ASCIIValue.charAt(ASCIIValue.length() / 2));
+    }
 }
--- Hashing/src/hash/ModuloArithmetic.java
+++ Hashing/src/hash/ModuloArithmetic.java
@@ -2,4 +2,9 @@ package hash;
 
 public class ModuloArithmetic extends HashFunction {
 
+    @Override
+    public int hash(String key) {
+        String ASCIIValue = toASCIIValue(key);
+        return Math.toIntExact(Long.parseLong(ASCIIValue) % 47);
+    }
 }
