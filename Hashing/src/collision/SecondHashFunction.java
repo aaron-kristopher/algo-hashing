e911001557b0dd36b783efe6bd31b19b29978b88
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-19 10:04:00 +0800

    refactor: Improve collision resolution in SecondHashFunction

--- Hashing/src/collision/SecondHashFunction.java
+++ Hashing/src/collision/SecondHashFunction.java
@@ -8,7 +8,17 @@ public class SecondHashFunction implements CollisionResolution {
     @Override
     public int resolveCollision(int index, String hashedValue, SymbolTable symbolTable) {
         ModuloArithmetic moduloArithmetic = new ModuloArithmetic();
-        return moduloArithmetic.hash(hashedValue);
+        int step = moduloArithmetic.hash(hashedValue);
+        int newIndex = (index + step) % symbolTable.length();
+
+        int loops = 0; // Prevent infinite loop
+        while (!symbolTable.isCellEmpty(newIndex) || loops < 50) {
+            step += step;
+            newIndex = (newIndex + step) % symbolTable.length();
+            loops++;
+        }
+
+        return newIndex;
     }
 
 }
