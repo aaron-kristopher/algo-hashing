7fba84f3150911ff6faf8ef19796757e814aed0b
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-20 13:03:05 +0800

    refactor: Change parameter and variable name from hashedValue to key

--- Hashing/src/collision/SecondHashFunctionImpl.java
+++ Hashing/src/collision/SecondHashFunctionImpl.java
@@ -6,13 +6,13 @@ import hash.ModuloArithmeticImpl;
 public class SecondHashFunctionImpl implements CollisionResolutionStrategy {
 
     @Override
-    public int resolveCollision(int index, String hashedValue, DataTableStrategy symbolTable) {
+    public int resolveCollision(int index, String key, DataTableStrategy symbolTable) {
         ModuloArithmeticImpl moduloArithmetic = new ModuloArithmeticImpl();
-        int step = moduloArithmetic.hash(hashedValue);
+        int step = moduloArithmetic.hash(key);
         int newIndex = (index + step) % symbolTable.length();
 
         int loops = 0; // Prevent infinite loop
-        while (!symbolTable.isCellEmpty(newIndex) || loops < 50) {
+        while (!symbolTable.isCellEmpty(newIndex) && loops < 50) {
             step += step;
             newIndex = (newIndex + step) % symbolTable.length();
             loops++;
@@ -20,5 +20,4 @@ public class SecondHashFunctionImpl implements CollisionResolutionStrategy {
 
         return newIndex;
     }
-
 }
