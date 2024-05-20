d8cd5916816dcdb086bd6485c2d8880ac4dbe7b3
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-20 17:10:28 +0800

    refactor: Update Menu class to improve displayTable method

--- Hashing/src/gui/Menu.java
+++ Hashing/src/gui/Menu.java
@@ -1,6 +1,18 @@
 package gui;
 
+import datastructures.DataTableStrategy;
+
 public class Menu {
+
+    public static void displayHome() {
+        System.out.println("Welcome to the Hashing Program!");
+        System.out.println("This program will create a hash table based on the options you choose.");
+        System.out.println("You will be asked to enter a number of words, and then the words themselves.");
+        System.out.println("The program will then display the hash table.");
+        System.out.println("Let's get started!");
+        System.out.println();
+        System.out.print("(Press enter to continue...) ");
+    }
 
     public static void displayHashingMenu() {
         System.out.println();
@@ -23,7 +35,85 @@ public class Menu {
         System.out.println();
     }
 
+    public static void displayStrategy(String hashFunction, String collisionResolution) {
+        System.out.println();
+        System.out.printf("HASH FUNCTION: %s\t\tCOLLISION RESOLUTION: %s", hashFunction, collisionResolution);
+        System.out.println();
+        System.out.println();
+    }
+
     public static void displayCloseProgram() {
         System.out.println("Closing program...");
     }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void displayTable(DataTableStrategy table) {
+
+        if (table.getCollisionResolutionStrategy().equals("Bucket Chaining"))
+            displayBucket(table);
+        else
+            displayString(table);
+
+    }
+
+    private static void displayString(DataTableStrategy table) {
+        int longestWordLength = table.getLongestWordLength();
+        String bar = "-".repeat(longestWordLength);
+        String segment = String.format("+------+---%s---", bar);
+        String header = segment.repeat(5) + "+";
+        int indexCount = 0;
+
+        String[] indexDisplay = new String[5];
+        String[] wordDisplay = new String[5];
+
+        for (int i = 0; i < table.length() / 5; i++) {
+            System.out.println(header);
+            String word = "";
+            String padding = "";
+
+            indexCount = i;
+            for (int j = 0; j < 5; j++) {
+                indexDisplay[j] = indexCount < 10 ? String.format("|   %d  |", indexCount)
+                        : String.format("|  %d  |", indexCount);
+                indexCount += table.length() / 5;
+            }
+
+            String formatWord = "";
+            indexCount = i;
+            for (int k = 0; k < 5; k++) {
+                word = table.get(indexCount) != null ? table.get(indexCount) : " ";
+                padding = createPadding(word, longestWordLength);
+
+                formatWord = String.format("%s%s%s%s", padding, word, padding,
+                        (longestWordLength % 2 == 0) ? ((word.length() % 2 == 1) ? " " : "")
+                                : word.length() % 2 == 0 ? " " : "");
+
+                if (k == 4)
+                    formatWord += "|";
+                wordDisplay[k] = formatWord;
+                indexCount += table.length() / 5;
+            }
+
+            for (int l = 0; l < 5; l++) {
+                System.out.printf("%s%s", indexDisplay[l], wordDisplay[l]);
+            }
+
+            System.out.println();
+        }
+        System.out.println(header);
+        System.out.println();
+    }
+
+    private static String createPadding(String word, int longestWordLength) {
+        return " ".repeat(((longestWordLength - word.length()) / 2) + 3);
+    }
+
+    private static void displayBucket(DataTableStrategy table) {
+        System.out.println(table.toString());
+    }
+
 }
