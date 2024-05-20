2dbcd490f85a6fe2814ba900e248d4b0e111905b
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-20 13:05:30 +0800

    feature: Add display for hashFunction, collision, and option menu

--- Hashing/src/gui/Menu.java
+++ Hashing/src/gui/Menu.java
@@ -2,4 +2,28 @@ package gui;
 
 public class Menu {
 
+    public static void displayHashingMenu() {
+        System.out.println();
+        System.out.println("HASH FUNCTION MENU ");
+        System.out.println();
+        System.out.println("1. Add and Fold");
+        System.out.println("2. Digit Selection");
+        System.out.println("3. Midsquare");
+        System.out.println("4. Modulo Arithmetic");
+        System.out.println();
+    }
+
+    public static void displayCollisionResolutionMenu() {
+        System.out.println();
+        System.out.println("COLLISION RESOLUTION MENU:");
+        System.out.println();
+        System.out.println("1. Bucket Chaining");
+        System.out.println("2. Linear Probing");
+        System.out.println("3. Second Hash Function");
+        System.out.println();
+    }
+
+    public static void displayCloseProgram() {
+        System.out.println("Closing program...");
+    }
 }
