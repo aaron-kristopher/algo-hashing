6b38c361d88f558cdb16943c4e5523832b0d538a
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-05-22 22:10:31 +0800

    refactor: Update DigitSelectionImpl to use ASCIIValue instead of key

--- Hashing/src/hash/DigitSelectionImpl.java
+++ Hashing/src/hash/DigitSelectionImpl.java
@@ -3,8 +3,8 @@ package hash;
 public class DigitSelectionImpl extends HashFunctionStrategy {
 
     @Override
-    public int hash(String hashedValue) {
-        return Character.getNumericValue(hashedValue.charAt(0));
+    public int hash(String ASCIIValue) {
+        return Character.getNumericValue(ASCIIValue.charAt(0));
     }
 
     @Override
